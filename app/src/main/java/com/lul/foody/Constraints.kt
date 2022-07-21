c4cec3ed9141ed531062afb7f767a081791694f8
Rodolpho Alves <rrpa15@live.com>
2022-07-21 03:47:17 -0300

    iniciando client de comunicação com api

---
+++ app/src/main/java/com/lul/foody/Constraints.kt
@@ -0,0 +1,13 @@
+package com.lul.foody
+
+import android.content.Context
+import android.content.pm.PackageManager
+import android.content.res.Resources
+
+
+class Constraints {
+    companion object {
+        const val API_KEY = "A_KEY"
+        const val BASE_URL = "https://api.spoonacular.com/"
+    }
+}
---
+++ app/src/main/java/com/lul/foody/FoodRecipesApi.kt
@@ -0,0 +1,14 @@
+package com.lul.foody
+
+import com.lul.foody.models.FoodRecipe
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
+interface FoodRecipesApi {
+    @GET("recipes/complexSearch")
+    suspend fun getRecipes(
+        @QueryMap
+        queries: Map<String, String>
+    ): Response<FoodRecipe>
+}
